ee6c24b8df388a75a5528357722745c700258f53
BolleanCC <songtao19980820@outlook.com>
2025-10-06 11:25:32 -0600

    Added user card

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,20 @@
+import UserCard from "@/components/UserCard";
+
 const Adminpage = () => {
-    return <div>Adminpage</div>;
+    return <div className="p-4 flex gap-4 flex-col md:flex-row">
+        {/* {LEFT} */}
+        <div className="w-full lg:w-2/3">
+            {/* {USERCARDS} */}
+            <div className="flex justify-between gap-4 flex-wrap">
+                <UserCard type="students" />
+                <UserCard type="teachers" />
+                <UserCard type="parents" />
+                <UserCard type="staffs" />
+            </div>
+        </div>
+        {/* {RIGHT} */}
+        <div className="w-full lg:w-1/3">right</div>
+    </div>;
 };
 
 export default Adminpage;
