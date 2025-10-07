5090e093eda1de1f11184fc2b941dcb4fddc411a
BolleanCC <songtao19980820@outlook.com>
2025-10-07 10:55:00 -0600

    Added calendar

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -2,6 +2,7 @@ import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 import FinanceChart from "@/components/FinanceChart";
+import EventCalendar from "@/components/EventCalendar";
 const Adminpage = () => {
     return <div className="p-4 flex gap-4 flex-col md:flex-row">
         {/* {LEFT} */}
@@ -30,7 +31,9 @@ const Adminpage = () => {
             </div>
         </div>
         {/* {RIGHT} */}
-        <div className="w-full lg:w-1/3">right</div>
+        <div className="w-full lg:w-1/3 flex flex-col gap-4">
+            <EventCalendar />
+        </div>
     </div>;
 };
 
