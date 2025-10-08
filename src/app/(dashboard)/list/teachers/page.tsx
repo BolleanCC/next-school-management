5a338cc16ab1442202543f48e096066d011dcdb9
BolleanCC <songtao19980820@outlook.com>
2025-10-07 22:20:45 -0600

    Added pagination(just for design)

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,3 +1,4 @@
+import Pagination from "@/components/Pagination";
 import TableSearch from "@/components/TableSearch";
 import Image from "next/image";
 const TeachersListpage = () => {
@@ -7,9 +8,15 @@ const TeachersListpage = () => {
             <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
             <div className="flex flex-col md:flex-row items-center gap-4">
                 <TableSearch />
-                <div>
-                    <button>
+                <div className="flex items-center gap-4 self-end">
+                    <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
                         <Image src="/filter.png" alt="add" width={14} height={14} />
+                    </button>
+                    <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+                        <Image src="/sort.png" alt="add" width={14} height={14} />
+                    </button>
+                    <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+                        <Image src="/plus.png" alt="add" width={14} height={14} />
                     </button>
                 </div>
             </div>
@@ -19,9 +26,7 @@ const TeachersListpage = () => {
 
         </div>
         {/* {PAGINATION} */}
-        <div className="">
-
-        </div>
+        <Pagination />
     </div>;
 };
 
