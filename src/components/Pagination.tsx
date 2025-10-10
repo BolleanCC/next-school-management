8d06aa019cfca866a734f02d2b33a72745230a6a
BolleanCC <songtao19980820@outlook.com>
2025-10-10 09:51:31 -0600

    Added parents page

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,6 +1,6 @@
 "use client";
 const Pagination = () => {
-    return <div className="flex items-center justify-between text-gray-500">
+    return <div className="flex items-center justify-between text-gray-500 mt-4">
         <button disabled={true} className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 cursor-pointer:not-allowed">Prev</button>
         <div className="flex items-center gap-2">
             <button className="px-2 rounded-md bg-sky">1</button>
