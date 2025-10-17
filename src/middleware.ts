67e961970a06b1820e620ee8e7c9a9a0deb219a4
BolleanCC <songtao19980820@outlook.com>
2025-10-17 16:07:06 -0600

    Protecting routes in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,24 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { routeAccessMap } from './lib/settings';
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
-    return NextResponse.next();
-}
+const matchers = Object.keys(routeAccessMap).map((route) => ({
+    matcher: createRouteMatcher([route]),
+    allowedRoles: routeAccessMap[route],
+}));
+
+console.log(matchers);
+
+export default clerkMiddleware(async (auth, req) => {
+    const { sessionClaims } = await auth();
+    const role = (sessionClaims?.metadata as { role?: string })?.role;
+
+    for (const { matcher, allowedRoles } of matchers) {
+        if (matcher(req) && (!role || !allowedRoles.includes(role))) {
+            return NextResponse.redirect(new URL(`/${role || 'sign-in'}`, req.url));
+        }
+    }
+});
 
 export const config = {
     matcher: [
