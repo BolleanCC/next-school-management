0e71c5a861a945cad8a4c42f3a3e0635222ef9f1
BolleanCC <songtao19980820@outlook.com>
2025-10-17 19:56:46 -0600

    Added role conditions for announcement and event page

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -2,7 +2,7 @@ import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { eventsData, role } from "@/lib/data";
+import { currentUserId, role } from "@/lib/utils";
 import prisma from "@/lib/prisma";
 import { ITEMS_PER_PAGE } from "@/lib/settings";
 import Image from "next/image";
@@ -34,16 +34,16 @@ const columns = [
         accessor: "endTime",
         className: "hidden md:table-cell",
     },
-    {
+    ...(role === "admin" ? [{
         header: "Actions",
         accessor: "action",
-    },
+    }] : []),
 ];
 
 const renderRow = (item: EventList) => (
     <tr
         key={item.id}
-        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
     >
         <td className="flex items-center gap-4 p-4">{item.title}</td>
         <td>{item.class?.name || "-"}</td>
@@ -98,6 +98,15 @@ const EventListPage = async ({ searchParams }: { searchParams: Promise<{ [key: s
         }
     }
 
+    // Role conditions
+    const roleConditions = {
+        teacher: { lessons: { some: { teacherId: currentUserId! } } },
+        student: { students: { some: { id: currentUserId! } } },
+        parent: { students: { some: { parentId: currentUserId! } } },
+    }
+
+    query.OR = [{ classId: null }, { class: roleConditions[role as keyof typeof roleConditions] || {}, }]
+
     const [data, count] = await prisma.$transaction([
         prisma.event.findMany({
             where: query,
@@ -118,10 +127,10 @@ const EventListPage = async ({ searchParams }: { searchParams: Promise<{ [key: s
                 <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
                     <TableSearch />
                     <div className="flex items-center gap-4 self-end">
-                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
                             <Image src="/filter.png" alt="" width={14} height={14} />
                         </button>
-                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
                             <Image src="/sort.png" alt="" width={14} height={14} />
                         </button>
                         {role === "admin" && <FormModal table="event" type="create" />}
