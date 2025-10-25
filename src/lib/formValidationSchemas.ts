fd3218fd4b1b0f82d31b1e4a05d7ab574f18f2c9
BolleanCC <songtao19980820@outlook.com>
2025-10-24 19:17:45 -0600

    Added delete subject method

--- src/lib/formValidationSchemas.ts
+++ src/lib/formValidationSchemas.ts
@@ -1,9 +1,16 @@
 import { z } from "zod";
 
 export const subjectSchema = z.object({
+    id: z.coerce.number().optional(),
     name: z
         .string()
         .min(1, { message: "Subject name is required!" })
 });
 
 export type SubjectSchema = z.infer<typeof subjectSchema>;
+
+export const deleteSchema = z.object({
+    id: z.coerce.number()
+});
+
+export type DeleteSchema = z.infer<typeof deleteSchema>;
