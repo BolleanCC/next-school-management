f8e22055334bca165550d7287e81656438f46596
BolleanCC <songtao19980820@outlook.com>
2025-10-24 20:10:05 -0600

    Fixed parent page error

--- src/components/forms/StudentForm.tsx
+++ src/components/forms/StudentForm.tsx
@@ -32,10 +32,12 @@ const StudentForm = ({
     type,
     data,
     setOpen,
+    relatedData,
 }: {
     type: "create" | "update";
     data?: any;
     setOpen: Dispatch<SetStateAction<boolean>>;
+    relatedData?: any;
 }) => {
     const {
         register,
